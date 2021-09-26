1f5a8fffcb12a19731291aa574e85ed0cd98301a
i4210577 <90830668+i4210577@users.noreply.github.com>
2021-09-26 08:47:11 -0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,3 +13,6 @@
 # Modify default IP
 sed -i 's/10.10.10.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 sed -i 's/$1$WplwC1t5$HBAtVXABp7XbvVjG4193B.:18753:0:99999:7:::/$1$ttZeRDG3$V0pEG8rex5imt2uWJp3gO\/:18875:0:99999:7:::/g' package/base-files/files/etc/shadow
+# modify wifi ssid 
+sed -i 's/OpenWrt_2.4G/OpenWrt_5G/g'  package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i '185s/OpenWrt_5G/OpenWrt_2.4G/' package/kernel/mac80211/files/lib/wifi/mac80211.sh
