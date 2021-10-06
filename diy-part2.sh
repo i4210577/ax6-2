31265ead02fe2e3f22620c2a5a1e3c27788d254b
i4210577 <90830668+i4210577@users.noreply.github.com>
2021-10-06 02:56:44 -0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,3 +16,16 @@ sed -i 's/$1$WplwC1t5$HBAtVXABp7XbvVjG4193B.:18753:0:99999:7:::/$1$ttZeRDG3$V0pE
 # modify wifi ssid 
 sed -i 's/OpenWrt_2.4G/OpenWrt_5G/g'  package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i '185s/OpenWrt_5G/OpenWrt_2.4G/' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# themes添加（svn co 命令意思：指定版本如https://github）
+#git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
+#git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
+#git clone https://github.com/openwrt-develop/luci-theme-atmaterial.git package/luci-theme-atmaterial
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+#git clone https://github.com/Leo-Jo-My/luci-theme-opentomato.git package/luci-theme-opentomato
+#git clone https://github.com/Leo-Jo-My/luci-theme-argon-dark-mod.git package/luci-theme-argon-dark-mod
+
+# Add luci-theme-argon
+#git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon
+#git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config
+#rm -rf ../lean/luci-theme-argon
