4a2381c44966957b78a3ef7fb476c61f05a384b0
i4210577 <90830668+i4210577@users.noreply.github.com>
2021-10-22 03:11:54 -0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -41,8 +41,8 @@ git clone https://github.com/kiddin9/luci-app-dnsfilter.git package/luci-app-dns
 
 git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
 #VSSR
-#git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
-#git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
+git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
 
 #添加smartdns
 git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
