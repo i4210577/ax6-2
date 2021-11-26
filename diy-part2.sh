4c65781acbf44a261409041f3e1a3ffc15c2316b
i4210577 <90830668+i4210577@users.noreply.github.com>
2021-11-26 13:09:26 -0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,8 +14,8 @@
 sed -i 's/10.10.10.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 #sed -i 's/$1$WplwC1t5$HBAtVXABp7XbvVjG4193B.:18753:0:99999:7:::/$1$ttZeRDG3$V0pEG8rex5imt2uWJp3gO\/:18875:0:99999:7:::/g' package/base-files/files/etc/shadow
 # modify wifi ssid 
-sed -i 's/OpenWrt_2.4G/OpenWrt_5G/g'  package/kernel/mac80211/files/lib/wifi/mac80211.sh
-sed -i '185s/OpenWrt_5G/OpenWrt_2.4G/' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#sed -i 's/OpenWrt_2.4G/OpenWrt_5G/g'  package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#sed -i '185s/OpenWrt_5G/OpenWrt_2.4G/' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # themes添加（svn co 命令意思：指定版本如https://github）
 #git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
@@ -53,8 +53,8 @@ git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 
 #git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
 #VSSR
-git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
-git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
+#git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
+#git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
 
 #添加smartdns
 #git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
