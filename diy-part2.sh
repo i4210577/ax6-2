6acf4952ec759b6099163305548a1edb071d7cf4
i4210577 <90830668+i4210577@users.noreply.github.com>
2021-11-28 09:02:54 -0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -28,7 +28,7 @@ sed -i '185s/OpenWrt_5G/OpenWrt_2.4G/' package/kernel/mac80211/files/lib/wifi/ma
 
 #添加额外非必须软件包
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
-git clone https://github.com/garypang13/luci-app-bypass package/luci-app-bypass
+#git clone https://github.com/garypang13/luci-app-bypass package/luci-app-bypass
 #git clone https://github.com/vernesong/OpenClash.git package/OpenClash
 #git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 #git clone https://github.com/zzsj0928/luci-app-pushbot.git package/luci-app-pushbot
@@ -39,9 +39,9 @@ git clone https://github.com/garypang13/luci-app-bypass package/luci-app-bypass
 #git clone https://github.com/linkease/istore-ui.git package/istore-ui
 #git clone https://github.com/shadowsocks/openwrt-feeds.git package/feeds
 #git clone https://github.com/honwen/openwrt-dnsmasq-extra.git package/feeds/openwrt-dnsmasq-extra
-git clone https://github.com/peter-tank/luci-app-dnscrypt-proxy2.git package/luci-app-dnscrypt-proxy2
-git clone https://github.com/shadowsocks/openwrt-feeds.git package/libs
-git clone https://github.com/peter-tank/openwrt-minisign.git package/minisign
+#git clone https://github.com/peter-tank/luci-app-dnscrypt-proxy2.git package/luci-app-dnscrypt-proxy2
+#git clone https://github.com/shadowsocks/openwrt-feeds.git package/libs
+#git clone https://github.com/peter-tank/openwrt-minisign.git package/minisign
 
 #git clone https://github.com/sirpdboy/luci-app-netdata.git package/luci-app-netdata
 #git clone https://github.com/kiddin9/luci-app-dnsfilter.git package/luci-app-dnsfilter
